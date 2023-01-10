44ed54902430466e4ba1ba5eb459ccebf5a33420
guillermo <h.uhm292@gmail.com>
2023-01-10 15:18:11 +0100

    creado componente header

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+<template>
+<header>
+    <div id="img-fondo">
+        
+    </div>
+</header>
+</template>
+<style lang = "scss" scoped>
+    header{
+    display: flex;
+    margin: auto;
+    height:20vh;
+    width: 100%;
+    #img-fondo{
+        height: 70vh;
+        width: 100%;
+        background-image: url(../assets/img/pokemonbackimg.jpeg) ;
+        background-size: cover;
+    }
+}
+</style>
