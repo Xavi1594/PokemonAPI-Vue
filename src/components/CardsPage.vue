958731194b7a00ca409fd3911a2da773c8fd6809
Xavi1594 <xavi9415@hotmail.com>
2023-01-11 12:13:27 +0100

    prueba llamada api

--- src/components/CardsPage.vue
+++ src/components/CardsPage.vue
@@ -4,13 +4,13 @@ import { ref } from 'vue';
 // import { RouterLink } from 'vue-router';
 
 
-const pokemons = ref ([])
+const pokemons = ref ({})
 
 const getData = async () => {
     try {
     const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/')
  
-pokemons.value = data.results
+pokemons.value = data
 } catch(error) {
     console.log('error');
 }
@@ -19,12 +19,13 @@ getData()
 </script>
 
 <template>
+
  <ul>
     <li v-for="pokemon in pokemons"  
           :key="pokemon.id"
           :pokemon="pokemon"
         >
-          {{ pokemon.name , pokemon.id}}
+          {{ pokemon.name , pokemon.id, }}
         </li>
 
  </ul>
