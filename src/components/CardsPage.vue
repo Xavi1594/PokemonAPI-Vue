8c63d0c72acfde23f28086bff59a0bda8737deb5
Xavi1594 <xavi9415@hotmail.com>
2023-01-13 14:39:16 +0100

    mergeado

--- src/components/CardsPage.vue
+++ src/components/CardsPage.vue
@@ -21,68 +21,89 @@ export default {
 };
 </script>
 <template>
-  <body>
-    <h1>Pokedex</h1>
-    <div class="poke-container" id="poke-container">
-      
-      
-      
-      <ul>
-        <li class="pokemon" v-for="pokemon in pokemons" :key="pokemon.name">
-          {{ pokemon.name }}
-          <div class="img-container"> 
-            <img :src="pokemon.sprites?.front_default" />
-          </div>
-        </li>
-      </ul>
+  <div class="grid-container">
+    <div v-for="pokemon in displayPokemons" :key="pokemon.name" class="card-container">
+      <div :class="`card ${pokemon.types.map(type => type.type.name).join(' ')}`">
+        <h2 class="card-title" >{{ pokemon.name.toUpperCase() }}</h2>
+        <div class="card-info">
+          <p>ID: {{ pokemon.id }}</p>
+          <p>Tipo: {{ pokemon.types.map(type => type.type.name).join(', ') }}</p>
+        </div>
+        <img :src="pokemon.sprites?.other.home.front_default" class="card-image" />
+      </div>
     </div>
-  </body>
-    </template>
-    <style lang="scss" scoped>
-    body {
-  background: #efefbb;
-  background: linear-gradient(to right, #d4d3dd, #efefbb);
-  font-family: 'Lato', sans-serif;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
+  </div>
+</template>
+<style>
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 1rem;
+    justify-content: center;
+    background-color: #A37F60;
 }
-h1 {
-  letter-spacing: 3px;
+.card-container {
+display: flex;
+flex-wrap: wrap;
 }
-.poke-container {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: space-between;
-  justify-content: center;
-  margin: 0 auto;
-  max-width: 1200px;
+.card {
+width: 14rem;
+height: 17rem;
+border: 1px solid black;
+padding: 1rem;
+box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.3);
+font-size: 12px;
+border-radius: 10%;
 }
-.pokemon {
-  background-color: #eee;
-  border-radius: 10px;
-  box-shadow: 0 3px 15px rgba(100, 100, 100, 0.5);
-  margin: 10px;
-  padding: 20px;
-  text-align: center;
+
+
+
+.card-title {
+text-align: center;
 }
-.pokemon .img-container {
-  background-color: rgba(255, 255, 255, 0.6);
-  border-radius: 50%;
-  width: 120px;
-  height: 120px;
-  text-align: center;
-  ;
+.card-title:hover{
+letter-spacing: 1px;
+text-shadow: 5px black;
 }
- .img-container img {
-  align-items: center;
+.card-info {
+text-align: center;
+font-size: 12px;
+}
+.card-image {
+width: 100%;
+max-width: 12rem;
+max-height: 12rem;
+cursor: pointern;
+}
+.card-image:hover {
+rotate: 20deg;
 
 }
-.pokemon .img-container img {
-  max-width: 90%;
-  margin-top: 20px;
-} 
+.grass {background:rgb(3, 139, 44) !important;}
+.poison {background:rgb(74, 7, 105) !important;}
+.water {background:rgb(8, 135, 219) !important;}
+.dragon {background:rgb(27, 2, 68) !important;}
+.ice {background:rgb(78, 199, 255) !important;}
+.flying {background:rgb(145, 215, 255) !important;}
+.fire {background:rgb(238, 135, 17) !important;}
+.ghost {background:rgb(74, 52, 87) !important;}
+.fighting {background:rgb(122, 0, 0) !important;}
+.normal {background:rgb(104, 104, 104) !important;}
+.psychic {background:rgb(195, 0, 255) !important;}
+.bug {background:rgb(52, 87, 6) !important;}
+.dark {background:rgb(43, 43, 43) !important;}
+.steel {background:rgb(116, 116, 116) !important;}
+.fairy {background:rgb(248, 165, 237) !important;}
+.eletric {background:rgb(255, 217, 1) !important;}
+.rock {background:rgb(88, 95, 100) !important;}
+.ground {background:rgb(92, 70, 70) !important;}
+.card-image {
+width: 100%;
+max-width: 12rem;
+max-height: 12rem;
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+}
 </style>
->>>>>>> 6db3eea4a73daa5f744a58854d42e68e998ec96b
